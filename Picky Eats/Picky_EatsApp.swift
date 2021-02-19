6de511cf63a4d8940b9ce3f8e2ec6716e7d4151a
Dylan Westfall <dwestfall@solstice.com>
2021-02-19 17:42:31 -0600

    Initial Commit

---
+++ Picky Eats/Picky_EatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picky_EatsApp.swift
+//  Picky Eats
+//
+//  Created by Dylan Westfall on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Picky_EatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
